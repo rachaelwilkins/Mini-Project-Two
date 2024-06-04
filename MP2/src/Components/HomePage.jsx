a16fda279253a479de9b505d1de388e8ee15fedc
rachaelwilksin <rachaelelizabethwilkins@outlook.com>
2024-06-05 11:40:02 +1200

    WIP

--- MP2/src/Components/HomePage.jsx
+++ MP2/src/Components/HomePage.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import theme from './Theme';
 
 //import myImage from '../Images/hero image.png';//
 
@@ -11,6 +10,7 @@ const HomePage = () => {
     <div className="App">
       <h1>World Clock</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam commodo blandit lobortis. Ut in elementum massa. Cras ut posuere tellus. Quisque elementum pellentesque congue. Vestibulum sit amet lectus viverra justo sodales maximus</p>
+      <button class="button"><a href="./WorldClock.jsx">Find the time</a></button>
     </div>
   );
 };
