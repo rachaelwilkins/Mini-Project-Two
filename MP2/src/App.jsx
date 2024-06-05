62894e7f4ef20d9c435c978a11073cd96cdde467
rachaelwilksin <rachaelelizabethwilkins@outlook.com>
2024-06-05 15:32:39 +1200

    WIP

--- MP2/src/App.jsx
+++ MP2/src/App.jsx
@@ -3,13 +3,15 @@ import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import './App.css';
 import './Components/Login'
-import theme from './Components/Theme'
-import ButtonAppBar from './Components/NavBar';
+// import theme from './Components/Theme'
+// import ButtonAppBar from './Components/NavBar';
+import WorldClock from './Components/WorldClock';
+
 
 function App() {
   return (
-
-  );
+    <WorldClock/>
+  )
 }
 
 export default App;
--- MP2/src/Components/WorldClock.jsx
+++ MP2/src/Components/WorldClock.jsx
@@ -2,25 +2,26 @@ import { useState, useEffect } from "react";
 import { PiSunFill } from "react-icons/pi";
 import { FaMoon } from "react-icons/fa";
 import { FaSearch } from "react-icons/fa";
-
+import axios from "axios";
 
 function WorldClock() {
-
+  let OK=false
   const [data, setData] = useState("");
   const [searchCity, setSearchCity] = useState("");
 
-  function getTime() {
-    Axios.get(
-      `https://worldtimeapi.org/api/timezone`
+  useEffect(()=>{
+    axios.get(
+      `http://worldtimeapi.org/api/ip`
     ).then((response) => {
-      setData(response.data[0]);
+      console.log(response.data);
+      setData(response.data);
     });
-  }
+  },[searchCity]) 
   
   return (
-    <div className="App">
+    <div>
       <h1>World Clock</h1>
-      <div className="searchBox">
+      {/* <div className="searchBox"> */}
         <input
           type="text"
           placeholder="Search city here..."
@@ -28,29 +29,19 @@ function WorldClock() {
             setSearchCity(e.target.value);
           }}
         />
-        <button
-          onClick={() => {
-            getTime();
-          }}
-        >
-          <FaSearch size="20px" />
-        </button>
-      </div>
-      <div>
-      <script>
-      </script>
-      </div>
+      {OK?"OK":"Not OK"}
+      {/* </div> */}
 
      // 
 
-      {data && (
+      {/* {data && (
         <div className="showResults">
         <h1>The time in {searchCity} is currently ${data[i].datetime}, ${data[i].day_of_week} June ${data[i].day_of_year}, 2024.</h1>   
         </div>
           
         // TO DO: Need to see if there is a way to display data as Saturday 2 June, 2024 for example. Figure out 'if' function for displaying sun and moon icon. 
   
-      )}
+      )} */}
     </div>
   );
 }
